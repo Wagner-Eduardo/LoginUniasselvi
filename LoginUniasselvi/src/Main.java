535977289d964756bc8d8b8521006e011598d8f5
Wagner Eduardo <fulllinux@hotmail.com>
2020-04-05 17:13:21 -0300

    Inclusão das classes bases do app LoginUniasselvi

---
+++ LoginUniasselvi/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		PessoaFisica pessoa1Pessoa = new PessoaFisica();
+		pessoa1Pessoa.setNome("Wagner");
+		pessoa1Pessoa.setCpf("28372263778");
+		/**/
+		System.out.println( pessoa1Pessoa.getNome() ); 
+	}
+
+}
